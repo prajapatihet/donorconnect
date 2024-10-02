3054cb917b5d3365d2ce36d8f913d534f9202a5f
Arindam <17arindambera@gmail.com>
2024-10-02 09:56:53 +0530

    splash screen created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+void main() async{
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(
+    Duration(seconds: 3),
+  );
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
