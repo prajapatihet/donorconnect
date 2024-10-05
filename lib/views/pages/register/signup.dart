e5ced099027cedc5cac52a0468fd310e50930584
Upendra Raj Joshi <upendraofficial10@gmail.com>
2024-10-05 17:09:21 +0545

    Add Navigate Back button on Sign Up Page inside the Body with comments removed

--- lib/views/pages/register/signup.dart
+++ lib/views/pages/register/signup.dart
@@ -72,26 +72,6 @@ class _SignuppageState extends State<Signuppage> {
         fontSize: 18,
         fontWeight: FontWeight.w500);
     return Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: const Color.fromARGB(243, 222, 221, 255),
-      //   leading:Builder(
-      //     builder: (BuildContext context) {
-      //       return Container(
-      //         decoration: BoxDecoration(
-      //           color: Colors.white.withOpacity(0.2),
-      //           shape: BoxShape.circle,
-      //         ),
-      //         child: IconButton(
-      //           icon: const Icon(Icons.arrow_back),
-      //           color: Colors.black,
-      //           onPressed: () {
-      //             Navigator.pop(context);
-      //           },
-      //         ),
-      //       );
-      //     },
-      //   ),
-      // ),
       body: Stack(
         children: [
           // BACKGROUND IMAGE
