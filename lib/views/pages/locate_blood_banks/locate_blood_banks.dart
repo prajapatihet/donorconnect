7a931d1dd197935fdb5afdf151896c6c74cb2a6a
Satyam Jha <satyamj210@gmail.com>
2024-10-09 02:34:13 +0530

    ios_build_fix

---
+++ lib/views/pages/locate_blood_banks/locate_blood_banks.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LocateBloodBanks extends StatefulWidget {
+  const LocateBloodBanks({super.key});
+
+  @override
+  State<LocateBloodBanks> createState() => _LocateBloodBanksState();
+}
+
+class _LocateBloodBanksState extends State<LocateBloodBanks> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
